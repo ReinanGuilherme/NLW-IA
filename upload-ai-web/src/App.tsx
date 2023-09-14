37324293ae30813e1df35587ab08bf634686a668
ReinanGuilhermeOffice <reinan.neto@grupofasitec.com.br>
2023-09-14 16:47:54 -0300

    atualizando

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,7 +1,17 @@
+import { Button } from "./components/ui/button";
 
 export function App() {
 
   return (
-   <h1>Hello World</h1>
+   <div>
+    <div className="px-6 py-3 flex items-center justify-between border-b ">
+      <h1 className="">upload.ai</h1>
+
+      <div>
+        <span>Desenvolvido com ❤️ no NLW da rocketseat.</span>
+        <Button>Github</Button>
+      </div>
+    </div>
+   </div>
   )
 }
